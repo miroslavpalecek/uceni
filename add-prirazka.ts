1c272899fb1140d5f5617fcdcbdb4b6eaa537d19
miroslavpalecek <miroslavpalecek@gmail.com>
2020-11-18 12:56:11 +0100

    mensi uprava funkce

--- add-prirazka.ts
+++ add-prirazka.ts
@@ -1,6 +1,6 @@
 const PRIRAZKA = 10;
 
-export function addPrirazka(a:number) {
+export function addPrirazka(a:number):number {
     return a + PRIRAZKA;
 }
 
--- secti_Cisla.ts
+++ secti_Cisla.ts
@@ -1,4 +1,4 @@
-export function sectiCisla(a:number, b:number) {
+export function sectiCisla(a:number, b:number):number {
     return a + b ;
 }
 //console.log(sectiCisla(5, 5));
